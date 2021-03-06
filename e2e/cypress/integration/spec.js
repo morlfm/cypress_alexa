7b077d3075f0dbb282f8d5ccbf7fc40fadfd2fb5
francisco <franciscomoral@hotmail.com>
2021-03-06 19:02:36 -0300

    correcting it

--- e2e/cypress/integration/spec.js
+++ e2e/cypress/integration/spec.js
@@ -1,5 +1,5 @@
-describe('compras', () => {
-  it('comprar livro', () => {
+describe('compras',() => {
+  it('login',() =>{
 
     cy.visit('https://www.amazon.com.br/')
     cy.get('#nav-signin-tooltip > a > span').click()
@@ -7,21 +7,27 @@ describe('compras', () => {
     cy.get('#continue').click()
     cy.get('#ap_password').type('Navarro030303')
     cy.get('#signInSubmit').click()
-    cy.get('#twotabsearchtextbox')
-    .type('1984 edicao especial').type('{enter}')
-    cy.get('a').contains('1984 - Edição especial').click()
-    cy.get('#add-to-cart-button', {timeout: 7000}).should('have.length', 1).click()
+    })
 
+    it('choose item',() =>{
+    cy.get('#twotabsearchtextbox')
+    .type('1984').type('{enter}')
+    cy.get('a').contains('1984').click()
+    })
+
+    it('add cart',() =>{
+    cy.get('#add-to-cart-button', {timeout: 7000}).should('have.length', 1).click()
     cy.get('#nav-cart').click()
+    })
+
+    it('place order',() =>{
     cy.get('#sc-buy-box-ptc-button > span > input').click()
     cy.get('#placeYourOrder > span > input').click()
     //cy.scrollTo('bottom')
     //cy.get('#pp-baNuhP-157').click()
-
-    cy.exec('bash /Users/morlfm/teste/cypress.sh',{timeout: 50000000})
-
-
-
-
-  })
+    })
+    // cancel order on Alexa
+    //it('Alexa cancel order',() =>{
+    //cy.exec('bash /Users/morlfm/teste/cypress.sh',{timeout: 50000000})
+    //})
 })
